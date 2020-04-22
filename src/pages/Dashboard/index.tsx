1c76a41e64961f14722554882f634153a67ca367
Jaime Barbosa <jaimebs@gmail.com>
2020-04-22 15:18:24 -0300

    refactor: Dashboard.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,30 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 import logo from '../../assets/logo-github_explore.svg';
+import { Title, Form, Repositories, Error } from './styles';
 
-import { Title, Form, Repositories } from './styles';
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const localStorageRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories'
+    );
+
+    if (localStorageRepositories) {
+      return JSON.parse(localStorageRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories)
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o usuário/repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+      const { data } = response;
+
+      setRepositories([...repositories, data]);
+      setNewRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('O repositório não existe!');
+    }
+  }
+
   return (
     <>
       <img src={logo} alt="Github" />
       <Title>Explore Repositórios no Github</Title>
 
-      <Form>
-        <input type="text" placeholder="Digite o usuario/repositório" />
+      <Form onSubmit={handleAddRepository} hasError={!!inputError}>
+        <input
+          type="text"
+          placeholder="Digite o usuario/repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <button type="submit">PESQUISAR</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/17276957?s=460&u=910b24d7b50d03b485ecd27ba68866b361c1ee83&v=4"
-            alt="Imagem Profile"
-          />
-        </a>
-        <div>
-          <strong>jaimebs/be-the-hero</strong>
-          <p>Qualquer coisa</p>
-        </div>
+        {repositories.map((repo) => (
+          <a key={repo.full_name} href="teste">
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+
+            <div>
+              <strong>{repo.full_name}</strong>
+              <p>{repo.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
+interface FormProps {
+  hasError: boolean;
+}
+
 export const Title = styled.h1`
   color: #3a3a3a;
   font-size: 48px;
@@ -9,7 +13,7 @@ export const Title = styled.h1`
   line-height: 56px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -22,6 +26,8 @@ export const Form = styled.form`
     border: 0;
     color: #3a3a3a;
     border-radius: 5px 0 0 5px;
+    border: 2px solid ${(props) => (props.hasError ? '#c53030' : '#fff')};
+    border-right: 0;
 
     &::placeholder {
       color: #a8a8b3;
@@ -45,13 +51,65 @@ export const Form = styled.form`
 `;
 
 export const Repositories = styled.div`
-  display: flex;
+  margin-top: 80px;
+  max-width: 700px;
 
-  img {
-    border-radius: 50%;
-  }
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
 
-  div {
-    margin-left: 20px;
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 64px;
+      height: 64;
+      border-radius: 50%;
+    }
+
+    div {
+      flex: 1;
+      margin: 0 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+
+    /* Tip: Modificar somente o segundo elemento em diante.
+       O & significa o próprio elemento.
+     */
+    & + a {
+      margin-top: 16px;
+    }
+
+    /* Transações e efeitos */
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
